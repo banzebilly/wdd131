09847a2f5b1a38f2feb272b920e13a8c3f8a3d55
banzebilly <bbanze49@gmail.com>
2026-06-29 21:58:30 +0200

    added index.html, styles folder and base.css and images and scripts folder and getdate.js file

---
+++ JAVASCRIPT-PRACTICE/test.js
@@ -0,0 +1,9 @@
+const buttonB = document.querySelector("#button_B");
+const headingB = document.querySelector("#headiing_B");
+
+
+
+buttonB.onclick  = () => {
+    buttonB.textContent = "Try me";
+    headingB.textContent = "1 Click so far";
+}
